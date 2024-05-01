a919345619ad31910e9dc98415fb16ea63895ddd
becouif <lovguya@gmail.com>
2024-05-01 21:29:02 +0300

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,10 @@ import "./Assets/footer.css";
 function Footer(){
   const time = new Date().getFullYear();
   return (
-    <footer>copyright &#169; {time} by<a href="https://github.com/Becouif"> Becouif</a> All Rights Reserved</footer>
+    <div className="footer-container">
+          <footer>copyright &#169; {time} by<a href="https://github.com/Becouif"> Becouif</a> All Rights Reserved</footer>
+    </div>
+
   )
 }
 
